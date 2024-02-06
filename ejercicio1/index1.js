fb96b047767aca80036d5158caf90d4cb9d24377
alex17nuro <alex17nuro@hotmail.com>
2024-02-06 01:10:03 +0100

    ejercicio1

--- ejercicio1/index1.js
+++ ejercicio1/index1.js
@@ -6,3 +6,15 @@ const arealateral = 23 * 13;
 const areaBase = 16 * 23;
 const areaTotal = arealateral + 2*areaBase;
 console.log('areaTotal', areaTotal + 'cm2');
+
+
+// Generalizamos
+const base = 14;
+const altura = 4;
+const ancho = 15;
+
+const volumen2 = base * altura * ancho;
+console.log('volumen2', volumen2);
+
+const area2 = base * altura + 2 * base * ancho;
+console.log('area2', area2);
