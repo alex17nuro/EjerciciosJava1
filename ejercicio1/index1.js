ffc9f01d1e986f8bd2e5a1c0c28244fd76678f0c
alex17nuro <alex17nuro@hotmail.com>
2024-02-06 01:01:57 +0100

    ejercicio1

---
+++ ejercicio1/index1.js
@@ -0,0 +1,8 @@
+
+const volumen = 16 * 23 * 13
+console.log('volumen', volumen + ' cm3');
+
+const arealateral = 23 * 13;
+const areaBase = 16 * 23;
+const areaTotal = arealateral + 2*areaBase;
+console.log('areaTotal', areaTotal + 'cm2');
